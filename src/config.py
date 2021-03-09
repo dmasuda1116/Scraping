9a0061532a5dd1ba5b72f8bff9cd99f6ceb641c7
Daiki Masuda <>
2021-03-09 14:19:57 +0900

    modify driver_path

--- src/config.py
+++ src/config.py
@@ -5,3 +5,4 @@ class Config:
     def __init__(self, json_path="../json/config.json"):
         config = json.load(open(json_path, "r"))
         self.access_token = config["access_token"]
+        self.driver_path = config["driver_path"]
--- src/main.py
+++ src/main.py
@@ -6,8 +6,12 @@ from selenium.webdriver.chrome.options import Options
 from Line import LINE
 from config import Config
 
+def list_dif(list_1,list_2,name):
+    first = [name + i.replace('を獲得', 'が復活しました(^^)') for i in list(set(list_1) -set(list_2))]
+    second = [name + i.replace('を獲得', 'の配布は終了しました>_<') for i in list(set(list_2) -set(list_1))]
+    return [first,second]
 
-def jalan_coupon(key):
+def jalan_coupon():
     #初期化
     all_coupon = []
     specific_coupon = []
@@ -19,20 +23,22 @@ def jalan_coupon(key):
     #ブラウザ監視
     while True:
       #全施設クーポン
-      all_coupon = coupon_check(all_coupon,all_facilities_url,"全国",key)
+      all_coupon = coupon_check(all_coupon,all_facilities_url,"全国")
       time.sleep(5)
       #施設限定クーポン
-      specific_coupon = coupon_check(specific_coupon, specific_facility_url,"施設限定",key)
+      specific_coupon = coupon_check(specific_coupon, specific_facility_url,"施設限定")
       time.sleep(5)
 
- 
-def coupon_check(status_before, url, name,key):
+def coupon_check(status_before, url, name):
+    #初期設定
+    config = Config()
+
     #ブラウザ非表示モード
     options = Options()
     options.add_argument('--headless')
 
     #クーポンページを取得
-    driver = webdriver.Chrome('../chromedriver', options=options)
+    driver = webdriver.Chrome(config.driver_path, options=options)
     driver.get(url) 
 
     #獲得可能クーポン状況を取得
@@ -40,19 +46,16 @@ def coupon_check(status_before, url, name,key):
     status = list(map(lambda c: c.text, log))
     driver.quit()
 
-    #クーポン状況に変更があれば
-    if status != status_before:
-      #獲得可能クーポンがあれば
-      if len(status) != 0:
-          for j in range(len(status)):
-              print(name +status[j].replace('を獲得', 'が復活しました(^^)'))
-              LINE(name+status[j].replace('を獲得', 'が復活しました(^^)'),key)
+    #クーポン復活の通知
+    dif = list_dif(status,status_before,name)[0]
+    if len(dif) != 0:
+        for i in range(len(dif)):
+            LINE(dif[i], config.access_token)
+
     return status
 
 def main():
-  config = Config()
-  KEY = config.access_token
-  jalan_coupon(KEY)
+    jalan_coupon()
 
 if __name__ == '__main__':
-  main()
+    main()
